4a0d411ad5b1d6eebec2667a0a5b2c7cb749dfc8
Yoann DEWILDE <dewilde.e1900976@etud.univ-ubs.fr>
2023-02-28 20:58:17 +0100

    Count while loops

--- yoann/src/main.rs
+++ yoann/src/main.rs
@@ -20,7 +20,11 @@ static CASE_YOANN3: &'static str = r#"while (true) {
     while (variab) {
         g();
     }
+
+    do {} while(x);
 }"#;
+
+// NOTE: do {} while() == do_statement sur tree sitter
 
 fn main() {
     let tree = JavaTreeGen::tree_sitter_parse(CASE_YOANN3.as_bytes()).unwrap_or_else(|t| t);
@@ -30,7 +34,8 @@ fn main() {
     println!("===========");
 
     let mut cursor = tree.walk();
-    walk(&mut cursor);
+    let count = find_while(&mut cursor, 0);
+    println!("{}", count);
 }
 
 fn walk(cursor: &mut tree_sitter::TreeCursor) {
@@ -45,4 +50,25 @@ fn walk(cursor: &mut tree_sitter::TreeCursor) {
     } 
     cursor.goto_parent();
     
+}
+
+fn find_while(cursor: &mut tree_sitter::TreeCursor, mut while_count: i32) -> i32 {
+    if cursor.node().kind() == "while_statement" {
+        println!("Node: {}", cursor.node().kind());
+        while_count = 1;
+    } else {
+        while_count = 0;
+    }
+
+
+    if cursor.goto_first_child() {
+        while_count += find_while(cursor, while_count);
+    } 
+
+    while cursor.goto_next_sibling() {
+        while_count += find_while(cursor, while_count);
+    } 
+    cursor.goto_parent();
+    
+    while_count
 }

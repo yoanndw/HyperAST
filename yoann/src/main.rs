f76bd502579472702e254bf9708f0b42c7a3dc84
Yoann DEWILDE <yoanndewilde@gmail.com>
2023-03-14 20:28:49 +0100

    [NOT WORKING] Iterate on a HyperAST taken from Git

--- yoann/src/main.rs
+++ yoann/src/main.rs
@@ -18,51 +18,51 @@ use hyper_ast_gen_ts_java::legion_with_refs::{
     print_tree_ids, print_tree_syntax, print_tree_syntax_with_ids, JavaTreeGen, TTreeCursor,
 };
 
-static CASE_YOANN1: &'static str = r#"if (1 < 2) {
-    f();
-    while (true) {}
-} else {
-    while (1 == 1)
-        while (false)
-            g();
-}"#;
+// static CASE_YOANN1: &'static str = r#"if (1 < 2) {
+//     f();
+//     while (true) {}
+// } else {
+//     while (1 == 1)
+//         while (false)
+//             g();
+// }"#;
 
-static CASE_YOANN2: &'static str = r#"(1 < 2)"#;
+// static CASE_YOANN2: &'static str = r#"(1 < 2)"#;
 
-fn main() {
-    let case = CASE_YOANN1;
-    let tree = JavaTreeGen::tree_sitter_parse(case.as_bytes()).unwrap_or_else(|t| t);
+// fn main() {
+//     let case = CASE_YOANN1;
+//     let tree = JavaTreeGen::tree_sitter_parse(case.as_bytes()).unwrap_or_else(|t| t);
 
-    println!("{}", tree.root_node().to_sexp());
+//     println!("{}", tree.root_node().to_sexp());
 
-    println!("===========");
+//     println!("===========");
 
-    let stores = Box::new(SimpleStores {
-        label_store: LabelStore::new(),
-        type_store: TypeStore {},
-        node_store: NodeStore::new(),
-    });
-    let md_cache = Box::new(Default::default());
-    let mut java_tree_gen = JavaTreeGen::new(Box::leak(stores), Box::leak(md_cache));
+//     let stores = Box::new(SimpleStores {
+//         label_store: LabelStore::new(),
+//         type_store: TypeStore {},
+//         node_store: NodeStore::new(),
+//     });
+//     let md_cache = Box::new(Default::default());
+//     let mut java_tree_gen = JavaTreeGen::new(Box::leak(stores), Box::leak(md_cache));
 
-    let full_node = java_tree_gen.generate_file(b"", case.as_bytes(), tree.walk());
-    let root = full_node.local.compressed_node as NodeIdentifier;
+//     let full_node = java_tree_gen.generate_file(b"", case.as_bytes(), tree.walk());
+//     let root = full_node.local.compressed_node as NodeIdentifier;
 
-    //walk_hast(java_tree_gen.stores, &StructuralPosition::new(root), root);
-    //walk_imp(java_tree_gen.stores, &StructuralPosition::new(root));
+//     //walk_hast(java_tree_gen.stores, &StructuralPosition::new(root), root);
+//     //walk_imp(java_tree_gen.stores, &StructuralPosition::new(root));
 
-    let walk_iter = HyperAstWalkIter::new(java_tree_gen.stores, &StructuralPosition::new(root));
-    // for cn in walk_iter {
-    //     println!("Iter type: {:?}", cn.get_type());
-    // }
+//     let walk_iter = HyperAstWalkIter::new(java_tree_gen.stores, &StructuralPosition::new(root));
+//     // for cn in walk_iter {
+//     //     println!("Iter type: {:?}", cn.get_type());
+//     // }
 
-    let num = walk_iter
-        .filter(|n| n.get_type() == Type::WhileStatement)
-        // .for_each(|nc| println!("{:?}", nc));
-        .count();
+//     let num = walk_iter
+//         .filter(|n| n.get_type() == Type::WhileStatement)
+//         // .for_each(|nc| println!("{:?}", nc));
+//         .count();
 
-    println!("Count while: {}", num);
-}
+//     println!("Count while: {}", num);
+// }
 
 type WalkStackElement = StructuralPosition;
 
@@ -101,5 +101,71 @@ impl<'a> Iterator for HyperAstWalkIter<'a> {
 
             Some(compressed_node)
         }
+    }
+}
+
+
+// use hyper_ast::{store::nodes::legion::HashedNodeRef, types::WithChildren};
+use hyper_ast_cvs_git::{preprocessed::PreProcessedRepository, git::fetch_github_repository};
+use hyper_diff::{
+    decompressed_tree_store::CompletePostOrder,
+    matchers::{
+        heuristic::gt::{greedy_subtree_matcher::{GreedySubtreeMatcher, SubtreeMatcher}},
+        mapping_store::{DefaultMultiMappingStore, VecStore},
+    },
+};
+
+use hyper_ast_benchmark_diffs::postprocess::print_mappings;
+
+// use hyper_ast_benchmark_diffs::print_mappings;
+
+fn main() {
+    // INRIA/spoon 7c7f094bb22a350fa64289a94880cc3e7231468f 78d88752a9f4b5bc490f5e6fb0e31dc9c2cf4bcd "spoon-pom" "" 2
+    let preprocessed = PreProcessedRepository::new("yoanndw/TPMaven");
+    let window_size = 2;
+    let mut preprocessed = preprocessed;
+    let (before, after) = (
+        "9acb418d7c750fce0924b2d71185f798d00c2bb0",
+        "9acb418d7c750fce0924b2d71185f798d00c2bb0"
+    );
+    let (before, after) = (
+        "HEAD",
+        "HEAD"
+    );
+    assert!(window_size > 1);
+
+    let processing_ordered_commits = preprocessed.pre_process_with_limit(
+        &mut fetch_github_repository(&preprocessed.name),
+        before,
+        after,
+        "/",
+        1000,
+    );
+    preprocessed.processor.purge_caches();
+    let c_len = processing_ordered_commits.len();
+    println!("Vec commits: {:?}", processing_ordered_commits);
+    let c = (0..c_len)
+        .map(|c| &processing_ordered_commits[c..(c + window_size).min(c_len)])
+        .next()
+        .unwrap();
+    let oid_src = &c[0];
+//  let oid_dst = &c[1];
+    let oid_dst = &c[0];
+
+    let commit_src = preprocessed.commits.get_key_value(&oid_src).unwrap();
+    let src_tr = commit_src.1.ast_root;
+    // let src_tr = preprocessed.child_by_name(src_tr, "hadoop-common-project").unwrap();
+
+    let commit_dst = preprocessed.commits.get_key_value(&oid_dst).unwrap();
+    let dst_tr = commit_dst.1.ast_root;
+    // let dst_tr = preprocessed.child_by_name(dst_tr, "hadoop-common-project").unwrap();
+    let stores = &preprocessed.processor.main_stores;
+
+    println!("========================");
+    // println!("Store: {:?}", stores);
+    let walk_iter = HyperAstWalkIter::new(stores, &StructuralPosition::new(src_tr));
+    for cn in walk_iter {
+        println!("Iter node: {:?}", cn);
+        println!("Iter type: {:?}", cn.get_type());
     }
 }

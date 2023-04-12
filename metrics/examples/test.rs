6c0f3664b09a0018b49fec4556b0cca6a630b475
Yoann DEWILDE <yoanndewilde@gmail.com>
2023-04-12 20:45:11 +0200

    Basic nodes count tests

--- metrics/examples/test.rs
+++ metrics/examples/test.rs
@@ -1,15 +1,7 @@
-use hyper_ast::store::{SimpleStores, TypeStore, nodes::DefaultNodeIdentifier as NodeIdentifier, nodes::DefaultNodeStore as NodeStore, labels::LabelStore};
 use hyper_ast_gen_ts_java::legion_with_refs::JavaTreeGen;
-use hyper_ast_metrics::{
-    count::count_while_statements, utils::hyper_ast_from_str, walk::HyperAstWalkIter,
-};
 
 fn main() {
-    let case = r#"while () {while ()}"#;
-    let case = r#"while () {
-                while () // ERROR
-            }"#;
-    let case = r#"do {} while ();"#;
+    let case = r#"call(1);"#;
 
     let tree = JavaTreeGen::tree_sitter_parse(case.as_bytes()).unwrap_or_else(|t| t);
 
